4cd5ad61adcb13283732a53541658ad0ebc89822
thanhnhannguyen308 <34601934+thanhnhannguyen308@users.noreply.github.com>
2019-11-01 22:34:04 +0700

    add models entity for project

---
+++ NNShop.Model/Abstract/IAudiable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NNShop.Model.Abstract
+{
+    public interface IAudiable
+    {
+        DateTime? CreateDate { set; get; }
+
+        string CreateBy { set; get; }
+
+        DateTime? UpdatedDate { get; set; }
+
+        string UpdatedBy { get; set; }
+
+        string MetaKeyword { set; get; }
+        string MetaDescription { set; get; }
+
+        bool Status { get; set; }
+
+    }
+}
