b85bb73023fea8d3b4de4cf073362a818c82ccf8
thanhnhannguyen308 <34601934+thanhnhannguyen308@users.noreply.github.com>
2019-11-02 12:20:01 +0700

    add migration and repository

---
+++ NNShop.Data/Repositories/ProductCategoryRepository.cs
@@ -0,0 +1,25 @@
+﻿using NNShop.Data.Infrastructure;
+using NNShop.Model.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NNShop.Data.Repositories
+{
+    public interface IProductCategoryRepository : IRepository<ProductCategory>
+    {
+        IEnumerable<ProductCategory> GetByAlias(string alias);
+    }
+
+    public class ProductCategoryRepository : RepositoryBase<ProductCategory>, IProductCategoryRepository
+    {
+        public ProductCategoryRepository(IDbFactory dbFactory)
+            : base(dbFactory)
+        {
+        }
+
+        public IEnumerable<ProductCategory> GetByAlias(string alias)
+        {
+            return this.DbContext.ProductCategories.Where(x => x.Alias == alias);
+        }
+    }
+}
