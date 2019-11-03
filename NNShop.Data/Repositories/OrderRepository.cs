b250ee85301408f0e0d102aefc6a92d2ccfe2442
thanhnhannguyen308 <34601934+thanhnhannguyen308@users.noreply.github.com>
2019-11-03 14:58:52 +0700

    change commit repository

--- NNShop.Data/Repositories/OrderRepository.cs
+++ NNShop.Data/Repositories/OrderRepository.cs
@@ -1,16 +1,10 @@
 ﻿using NNShop.Data.Infrastructure;
 using NNShop.Model.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NNShop.Data.Repositories
 {
     public interface IOrderRepository : IRepository<Order>
     {
-      
     }
 
     public class OrderRepository : RepositoryBase<Order>, IOrderRepository
@@ -18,7 +12,5 @@ namespace NNShop.Data.Repositories
         public OrderRepository(IDbFactory dbFactory) : base(dbFactory)
         {
         }
-
-       
     }
 }
--- NNShop.Data/Repositories/PostCategoryRepository.cs
+++ NNShop.Data/Repositories/PostCategoryRepository.cs
@@ -1,10 +1,5 @@
 ﻿using NNShop.Data.Infrastructure;
 using NNShop.Model.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NNShop.Data.Repositories
 {
