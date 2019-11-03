1491ded97d283024878a6a4e2e8d12e813621a71
thanhnhannguyen308 <34601934+thanhnhannguyen308@users.noreply.github.com>
2019-11-03 22:51:58 +0700

    commit more service

--- NNShop.Data/Repositories/PostRepository.cs
+++ NNShop.Data/Repositories/PostRepository.cs
@@ -1,11 +1,13 @@
 ﻿using NNShop.Data.Infrastructure;
 using NNShop.Model.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace NNShop.Data.Repositories
 {
     public interface IPostRepository : IRepository<Post>
     {
-        //IEnumerable<Post> GetAllByTag(string tag, int pageIndex, int pageSize, out int totalRow);
+        IEnumerable<Post> GetAllByTag(string tag, int pageIndex, int pageSize, out int totalRow);
     }
 
     public class PostRepository : RepositoryBase<Post>, IPostRepository
@@ -14,20 +16,20 @@ namespace NNShop.Data.Repositories
         {
         }
 
-        //public IEnumerable<Post> GetAllByTag(string tag, int pageIndex, int pageSize, out int totalRow)
-        //{
-        //    var query = from p in DbContext.Posts
-        //                join pt in DbContext.PostTags
-        //                on p.ID equals pt.PostID
-        //                where pt.TagID == tag && p.Status
-        //                orderby p.CreatedDate descending
-        //                select p;
+        public IEnumerable<Post> GetAllByTag(string tag, int pageIndex, int pageSize, out int totalRow)
+        {
+            var query = from p in DbContext.Posts
+                        join pt in DbContext.PostTags
+                        on p.ID equals pt.PostID
+                        where pt.TagID == tag && p.Status
+                        orderby p.CreateDate descending
+                        select p;
 
-        //    totalRow = query.Count();
+            totalRow = query.Count();
 
-        //    query = query.Skip((pageIndex - 1) * pageSize).Take(pageSize);
+            query = query.Skip((pageIndex - 1) * pageSize).Take(pageSize);
 
-        //    return query;
-        //}
+            return query;
+        }
     }
 }
